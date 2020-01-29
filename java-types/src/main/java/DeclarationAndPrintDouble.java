f5a402c2a654a688a1d27772d543d2fb662c39b7
Fabí Canedo Yugar <fabicaneyu@gmail.com>
2020-01-28 21:44:34 -0300

    2 classes

---
+++ java-types/src/main/java/DeclarationAndPrintDouble.java
@@ -0,0 +1,19 @@
+public class DeclarationAndPrintDouble {
+
+    public static void main(String args[]){
+        //Declarando e imprimindo double
+        doubleValue();
+    }
+    private static void doubleValue(){
+
+        System.out.println("[DECLARANDO E IMPRIMINDO NÚMEROS QUEBRADOS]");
+
+        int a = 10.20;
+        System.out.println(a);
+
+        int b=10.18, f = 10.44;
+
+        System.out.println("A:" + a + "B:"+b+f);
+
+    }
+}
---
+++ java-types/src/main/java/DeclarationAndPrintInt.java
@@ -0,0 +1,18 @@
+public class DeclarationAndPrint{
+    public static void main(String args[]){
+        //Declarando e imprimindo inteiros
+        intValue();
+    }
+    private static void intValue(){
+
+        System.out.println("[DECLARANDO E IMPRIMINDO INTEIROS]");
+
+        int i = 10;
+        System.out.println(i);
+
+        int j=10, x = 10;
+
+        System.out.println("J:" + j + ", X:"+x);
+
+    }
+}
